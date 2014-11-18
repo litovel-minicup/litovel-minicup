c5686028de3be8712cd8615bc3b9809f7fbbbeb3
Mrázek Tomáš <mrazekthom@gmail.com>
2014-11-18 21:32:15 +0100

    Fixed DateTime error

--- minicup/model/Entity/MatchTerm.php
+++ minicup/model/Entity/MatchTerm.php
@@ -3,12 +3,11 @@
 namespace Minicup\Model\Entity;
 
 use LeanMapper\Entity;
-use Nette\Utils\DateTime;
 
 /**
  * @property int       $id
- * @property DateTime  $start
- * @property DateTime  $end
+ * @property \DateTime $start
+ * @property \DateTime  $end
  * @property string    $location
  *
  */
