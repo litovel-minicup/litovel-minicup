bf4f457e14ac90767b0b130a8264acdb6050e820
Mrázek Tomáš <mrazekthom@gmail.com>
2014-11-18 19:45:22 +0100

    Finished base for TeamDetailComponent

--- minicup/components/TeamDetailComponent/TeamDetailComponent.php
+++ minicup/components/TeamDetailComponent/TeamDetailComponent.php
@@ -3,6 +3,7 @@
 namespace Minicup\Components;
 
 use Minicup\Model\Entity\Team;
+use Minicup\Model\Repository\TeamRepository;
 use Nette\Application\UI\Control;
 
 class TeamDetailComponent extends Control
@@ -11,14 +12,20 @@ class TeamDetailComponent extends Control
     /** @var Team */
     public $team;
 
-    public function __construct(Team $team)
-    {
+    /** @var \Minicup\Model\Repository\TeamRepository */
+    private $TR;
+
+    public function __construct(Team $team, TeamRepository $TR){
         parent::__construct();
         $this->team = $team;
+        $this->TR = $TR;
     }
 
     public function render(){
-        
+        $this->template->setFile(__DIR__ . '/TeamDetailComponent.latte');
+        $this->template->team = $this->team;
+        $this->template->TR = $this->TR;
+        $this->template->render();
     }
 
 }
