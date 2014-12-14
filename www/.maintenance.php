5ba12d37c2bcd735adac2ea04fb02eee133c0ed5
Joe Kolář <thejoeejoee@gmail.com>
2014-12-14 01:01:33 +0100

    Update .maintenance.php

--- www/.maintenance.php
+++ www/.maintenance.php
@@ -7,7 +7,6 @@ header('Retry-After: 300'); // 5 minutes in seconds
 <!DOCTYPE html>
 <meta charset="utf-8">
 <meta name="robots" content="noindex">
-<meta name="generator" content="Nette Framework">
 
 <style>
 	body { color: #333; background: white; width: 500px; margin: 100px auto }
@@ -15,11 +14,11 @@ header('Retry-After: 300'); // 5 minutes in seconds
 	p { font: 21px/1.5 Georgia,serif; margin: 1.5em 0 }
 </style>
 
-<title>Site is temporarily down for maintenance</title>
+<title>Aplikace se dočasně nedostupná</title>
 
-<h1>We're Sorry</h1>
+<h1>Pardon</h1>
 
-<p>The site is temporarily down for maintenance. Please try again in a few minutes.</p>
+<p>Webová aplikace je momentálně pod údržbou. Zkuste to prosím za chvíli.</p>
 
 <?php
 
