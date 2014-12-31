7f3c18da99d5d59323dfc4080d71ce8667d69564
Josef Kolář <thejoeejoee@gmail.com>
2014-12-31 17:00:29 +0100

    refactored routing, added support for 2 params

--- minicup/model/Repository/TeamRepository.php
+++ minicup/model/Repository/TeamRepository.php
@@ -2,17 +2,20 @@
 
 namespace Minicup\Model\Repository;
 
+use Minicup\Model\Entity\Category;
 use Minicup\Model\Entity\Team;
 
 class TeamRepository extends Repository
 {
     /**
      * @param $slug string
+     * @param $category Category
      * @return Team|NULL
      */
-    public function getBySlug($slug)
+    public function getBySlug($slug, Category $category)
     {
-        $row = $this->connection->select('*')->from($this->getTable())->where('slug = %s', $slug)->fetch();
+        $row = $this->connection->select('*')->from($this->getTable())->where('slug = %s', $slug, 'AND [category_id] = ', $category->id)->fetch();
         return $row ? $this->createEntity($row) : NULL;
     }
+
 }
--- minicup/router/RouterFactory.php
+++ minicup/router/RouterFactory.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace Minicup;
+namespace Minicup\Router;
 
 use Minicup\Model\Entity\Category;
 use Minicup\Model\Entity\Team;
 use Minicup\Model\Repository\CategoryRepository;
 use Minicup\Model\Repository\TeamRepository;
 use Nette\Application\IRouter;
+use Nette\Application\Request;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
 
@@ -45,34 +46,29 @@ class RouterFactory
 
         $front = new RouteList('Front');
 
-        $front[] = new Route('tymy[/<category>]', array(
+        $front[] = new FilterRoute('migrate', 'Homepage:migrate');
+        $front[] = new FilterRoute('tymy[/<category>]', array(
             'presenter' => 'Team',
             'action' => 'default',
             'category' => $categoryFilter
         ));
 
-        $front[] = new Route('zapasy[/<category>]', array(
+        $front[] = new FilterRoute('zapasy[/<category>]', array(
             'presenter' => 'Match',
             'action' => 'default',
             'category' => $categoryFilter
         ));
 
-        $front[] = new Route('<category>/<team>', array(
+        $front[] = (new FilterRoute('<category>/<team>', array(
             'presenter' => 'Team',
             'action' => 'detail',
-            'category' => $categoryFilter,
-            'team' => [
-                Route::FILTER_IN => function ($slug) use ($TR) {
-                    return $TR->getBySlug($slug);
-                },
-                Route::FILTER_OUT => function (Team $team) use ($TR) {
-                    return $team->slug;
-                }]
-        ));
+            'category' => $categoryFilter
+        )))->addFilter('team', array($this, 'teamSlug2Team'), array($this, 'team2TeamSlug'));
+
 
         $router = new RouteList();
         $router[] = $front;
-        $router[] = new Route('admin/<presenter>/<action>[/<id>]', array(
+        $router[] = new FilterRoute('admin/<presenter>/<action>[/<id>]', array(
             'module' => 'admin',
             'presenter' => 'Homepage',
             'action' => 'default'
@@ -86,6 +82,26 @@ class RouterFactory
         ));
 
         return $router;
+    }
+
+    /**
+     * @param $teamSlug string
+     * @param Request $request
+     * @return Team|NULL
+     */
+    public function teamSlug2Team($teamSlug, Request $request)
+    {
+        return $this->TR->getBySlug($teamSlug, $request->parameters['category']);
+    }
+
+    /**
+     * @param Team $team
+     * @param Request $request
+     * @return string
+     */
+    public function team2TeamSlug(Team $team, Request $request)
+    {
+        return $team->slug;
     }
 
 }
