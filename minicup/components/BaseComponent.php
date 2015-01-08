5d2a274100f0f337495a606ad834f45e5cce9896
Josef Kolář <thejoeejoee@gmail.com>
2015-01-08 21:23:00 +0100

    base comp: calling injects on base comp

--- minicup/components/BaseComponent.php
+++ minicup/components/BaseComponent.php
@@ -3,9 +3,31 @@
 namespace Minicup\Components;
 
 
+use Minicup\Forms\IFormFactory;
 use Nette\Application\UI\Control;
+use Nette\Application\UI\Presenter;
 
 class BaseComponent extends Control
 {
+    /** @var  IFormFactory */
+    protected $FF;
+
+    /**
+     * @param IFormFactory $FF
+     */
+    public function injectFF(IFormFactory $FF)
+    {
+        $this->FF = $FF;
+    }
+
+    protected function attached($presenter)
+    {
+        if ($presenter instanceof Presenter) {
+            // TODO: exists better solution for this problem?
+            $presenter->context->callInjects($this);
+        }
+        parent::attached($presenter);
+    }
+
 
 }
