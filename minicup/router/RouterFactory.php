2bf8f9ec019633b732ea7485d3a00dc85491f24a
Mrázek Tomáš <mrazekthom@gmail.com>
2015-01-14 11:09:16 +0100

    typos in router

--- minicup/router/RouterFactory.php
+++ minicup/router/RouterFactory.php
@@ -22,7 +22,7 @@ class RouterFactory extends Object
 
     /**
      * @param CategoryRepository $CR
-     * @param TeamRepository $TR
+     * @param TeamRepository     $TR
      */
     public function __construct(CategoryRepository $CR, TeamRepository $TR)
     {
@@ -47,38 +47,38 @@ class RouterFactory extends Object
         $front = new RouteList('Front');
         $front[] = new Route('', 'Homepage:default');
         $front[] = new Route('migrate', 'Homepage:migrate');
-        $front[] = new Route('tymy[/<category>]', array(
+        $front[] = new Route('tymy[/<category>]', [
             'presenter' => 'Team',
             'action' => 'default',
             'category' => $categoryFilter
-        ));
+        ]);
 
-        $front[] = new Route('zapasy[/<category>]', array(
+        $front[] = new Route('zapasy[/<category>]', [
             'presenter' => 'Match',
             'action' => 'default',
             'category' => $categoryFilter
-        ));
+        ]);
 
-        $front[] = (new FilterRoute('<category>/<team>', array(
+        $front[] = (new FilterRoute('<category>/<team>', [
             'presenter' => 'Team',
             'action' => 'detail',
             'category' => $categoryFilter
-        )))->addFilter('team', $this->teamSlug2Team, $this->team2TeamSlug);
+        ]))->addFilter('team', $this->teamSlug2Team, $this->team2TeamSlug);
 
 
         $router = new RouteList();
         $router[] = $front;
-        $router[] = new Route('admin/<presenter>/<action>[/<id>]', array(
+        $router[] = new Route('admin/<presenter>/<action>[/<id>]', [
             'module' => 'admin',
             'presenter' => 'Homepage',
             'action' => 'default'
-        ));
+        ]);
 
         return $router;
     }
 
     /**
-     * @param $teamSlug string
+     * @param         $teamSlug string
      * @param Request $request
      * @return Team|NULL
      */
