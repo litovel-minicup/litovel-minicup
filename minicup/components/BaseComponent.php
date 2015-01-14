2fb68c7a78412b73ca982362573674ddafc836fa
Josef Kolář <thejoeejoee@gmail.com>
2015-01-14 20:24:25 +0100

    added support for component views

--- minicup/components/BaseComponent.php
+++ minicup/components/BaseComponent.php
@@ -6,13 +6,14 @@ namespace Minicup\Components;
 use Minicup\Forms\IFormFactory;
 use Nette\Application\UI\Control;
 use Nette\Application\UI\Presenter;
+use Nette\Utils\Strings;
 
 class BaseComponent extends Control
 {
     /** @var  IFormFactory */
     protected $FF;
 
-    /** @var String|null  */
+    /** @var String|null */
     protected $view = NULL;
 
     /**
@@ -23,6 +24,9 @@ class BaseComponent extends Control
         $this->FF = $FF;
     }
 
+    /**
+     * @param $presenter
+     */
     protected function attached($presenter)
     {
         if ($presenter instanceof Presenter) {
@@ -32,17 +36,56 @@ class BaseComponent extends Control
         parent::attached($presenter);
     }
 
+    /**
+     * adding views
+     * @return \Nette\Application\UI\ITemplate
+     */
     protected function createTemplate()
     {
         $template = parent::createTemplate();
         $name = $this->reflection->shortName;
-        if ($this->view) {
-            $name .= '.'.$this->view;
-        }
         $dir = $this->presenter->context->parameters['appDir'];
-        $template->setFile("$dir/templates/components/$name.latte");
+        $paths = [];
+        if ($this->view) {
+            $view = $this->view;
+            $paths[] = "$dir/templates/components/$name/$view.latte";
+            $paths[] = "$dir/templates/components/$name.$view.latte";
+        } else {
+            $paths[] = "$dir/templates/components/$name/default.latte";
+            $paths[] = "$dir/templates/components/$name.latte";
+        }
+        foreach ($paths as $path) {
+            if (is_file($path)) {
+                $template->setFile($path);
+                return $template;
+            }
+        }
+        $template->setFile($path);
         return $template;
     }
 
+    /**
+     * render component
+     */
+    public function render()
+    {
+        $this->template->render();
+    }
 
+    /**
+     * @param string $name
+     * @param array $args
+     * @return mixed
+     */
+    public function __call($name, $args)
+    {
+        if (substr($name, 0, 6) == "render" && $name !== "render") {
+            $this->tryCall($name, $args);
+            $view = Strings::lower(Strings::substring($name, 6));
+            $this->view = $view;
+            $this->render();
+        } else {
+            return parent::__call($name, $args);
+        }
+    }
 }
