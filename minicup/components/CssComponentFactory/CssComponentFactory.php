c67f2031e0833bb864e21bfd45203ac431ca0af7
Josef Kolář <thejoeejoee@gmail.com>
2015-01-24 16:07:01 +0100

    added css minification depending on environment

--- minicup/components/CssComponentFactory/CssComponentFactory.php
+++ minicup/components/CssComponentFactory/CssComponentFactory.php
@@ -4,23 +4,32 @@ namespace Minicup\Components;
 
 
 use Nette\Object;
+use Nette\Utils\Finder;
 use WebLoader\Compiler;
 use WebLoader\FileCollection;
-use WebLoader\Filter\ScssFilter;
 use WebLoader\Nette\CssLoader;
 
+/**
+ * Factory for generating css component
+ * @package Minicup\Components
+ */
 class CssComponentFactory extends Object
 {
     /** @var  string */
     private $wwwPath;
 
+    /** @var  bool */
+    private $productionMode;
+
+
     /**
      * @param string $wwwPath
-     * @param ScssFilter $scssFilter
+     * @param bool $productionMode
      */
-    public function __construct($wwwPath)
+    public function __construct($wwwPath, $productionMode)
     {
         $this->wwwPath = $wwwPath;
+        $this->productionMode = $productionMode;
     }
 
     /**
@@ -29,14 +38,18 @@ class CssComponentFactory extends Object
     public function create()
     {
         $files = new FileCollection($this->wwwPath);
-        $files->addFile('scss/index.css');
-        $files->addFile('css/grido.css');
+        $files->addFile('assets/scss/index.css');
+        $files->addFiles(Finder::findFiles('*.css')->from($this->wwwPath.'/assets/css'));
 
-        $compiler = Compiler::createCssCompiler($files, $this->wwwPath . '/temp');
-
+        $compiler = Compiler::createCssCompiler($files, $this->wwwPath.'/temp');
         // TODO: add urls fixing
         //$compiler->addFilter(new CssUrlsFilter());
 
+        if ($this->productionMode) {
+            $compiler->addFilter(function ($code) {
+                return \CssMin::minify($code);
+            });
+        }
         $control = new CssLoader($compiler, '/temp');
         return $control;
     }
