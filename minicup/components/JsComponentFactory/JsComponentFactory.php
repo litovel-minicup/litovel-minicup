111ed02a36f22eaf46583cb996c67813f188c79f
Josef Kolář <thejoeejoee@gmail.com>
2015-01-24 17:57:15 +0100

    added javascript remote compiler

--- minicup/components/JsComponentFactory/JsComponentFactory.php
+++ minicup/components/JsComponentFactory/JsComponentFactory.php
@@ -3,11 +3,11 @@
 namespace Minicup\Components;
 
 
+use Closure\RemoteCompiler;
 use Nette\Object;
 use Nette\Utils\Finder;
 use WebLoader\Compiler;
 use WebLoader\FileCollection;
-use WebLoader\Nette\CssLoader;
 use WebLoader\Nette\JavaScriptLoader;
 
 /**
@@ -22,7 +22,6 @@ class JsComponentFactory extends Object
     /** @var  bool */
     private $productionMode;
 
-
     /**
      * @param string $wwwPath
      * @param bool $productionMode
@@ -34,18 +33,26 @@ class JsComponentFactory extends Object
     }
 
     /**
-     * @return CssLoader
+     * @return JavaScriptLoader
      */
     public function create()
     {
         $files = new FileCollection($this->wwwPath);
         $files->addRemoteFile('http://code.jquery.com/jquery-2.1.1.min.js');
         $files->addFile('assets/js/grido.js');
-        $files->addFiles(Finder::findFiles('*.js')->from($this->wwwPath.'/assets/js'));
+        $files->addFiles(Finder::findFiles('*.js')->from($this->wwwPath . '/assets/js'));
         $files->addFile('assets/js/main.js');
 
-        $compiler = Compiler::createJsCompiler($files, $this->wwwPath.'/temp');
+        $compiler = Compiler::createJsCompiler($files, $this->wwwPath . '/temp');
 
+        if ($this->productionMode) {
+            $compiler->addFilter(function ($code) {
+                $remoteCompiler = new RemoteCompiler();
+                $remoteCompiler->addScript($code);
+                $remoteCompiler->setMode(RemoteCompiler::MODE_SIMPLE_OPTIMIZATIONS);
+                return $remoteCompiler->compile()->getCompiledCode();
+            });
+        }
         $control = new JavaScriptLoader($compiler, '/temp');
         return $control;
     }
