153fda9233d9d65e9ba842270d2610a135444b0a
Josef Kolář <thejoeejoee@gmail.com>
2015-01-31 00:02:46 +0100

    removed duplication in list of teams with category table

--- minicup/components/ListOfTeamsComponent/ListOfTeamsComponent.php
+++ minicup/components/ListOfTeamsComponent/ListOfTeamsComponent.php
@@ -2,18 +2,10 @@
 
 namespace Minicup\Components;
 
-use LeanMapper\Exception\InvalidValueException;
 use Minicup\Model\Entity\Category;
-use Minicup\Model\Entity\Team;
-use Minicup\Model\Entity\TeamInfo;
 use Minicup\Model\Repository\CategoryRepository;
-use Minicup\Model\Repository\EntityNotFoundException;
 use Minicup\Model\Repository\TeamInfoRepository;
 use Minicup\Model\Repository\TeamRepository;
-use Nette\Application\UI\Form;
-use Nette\Application\UI\Multiplier;
-use Nette\Utils\ArrayHash;
-use Nextras\Forms\Rendering\Bs3FormRenderer;
 
 class ListOfTeamsComponent extends BaseComponent
 {
@@ -46,74 +38,4 @@ class ListOfTeamsComponent extends BaseComponent
         $template->render();
     }
 
-    /**
-     * @return Multiplier
-     */
-    protected function createComponentTeamEditForm()
-    {
-        $TR = $this->TR;
-        $FF = $this->FF;
-        $me = $this;
-        return new Multiplier(function ($categoryId) use ($TR, $FF, $me) {
-            return new Multiplier(function ($teamId) use ($TR, $FF, $categoryId, $me) {
-                $f = $FF->create();
-                $f->getElementPrototype()->class[] = 'form-inline';
-                $f->setRenderer(new Bs3FormRenderer());
-                $f->addText('name')->setRequired();
-                $f->addText('slug')->setRequired();
-                $f->addHidden('categoryId', $categoryId);
-                $f->addSubmit('submit', $teamId ? 'Editovat' : 'Přidat');
-                $f->onSuccess[] = $me->editFormSucceeded;
-                try {
-                    $team = $TR->get((int)$teamId);
-                } catch (EntityNotFoundException $e) {
-                    $team = NULL;
-                }
-                if ($team) {
-                    $f['name']->setValue($team->i->name);
-                    $f['slug']->setValue($team->i->slug);
-                    $f->addHidden('teamId')->setValue($team->id);
-                } else {
-                    $f->addHidden('teamId')->setValue(0);
-                }
-                return $f;
-            });
-        });
-    }
-
-    /**
-     * @param Form $form
-     * @param ArrayHash $values
-     * @throws EntityNotFoundException
-     * @throws InvalidValueException
-     */
-    public function editFormSucceeded(Form $form, ArrayHash $values)
-    {
-        $category = $this->CR->get($values['categoryId']);
-        $name = $values['name'];
-        $slug = $values['slug'];
-        $teamId = $values['teamId'];
-        if ($teamId) {
-            $teamInfo = $this->TR->get($teamId)->i;
-        } else {
-            $teamInfo = new TeamInfo();
-        }
-        $teamInfo->name = $name;
-        $teamInfo->slug = $slug;
-        $teamInfo->category = $category;
-        $this->TIR->persist($teamInfo);
-        if (!$teamId) {
-            $team = new Team();
-            $team->i = $teamInfo;
-            $team->actual = 1;
-            $team->category = $category;
-            $this->TR->persist($team);
-        }
-        if ($this->presenter->isAjax()) {
-            $this->redrawControl();
-        } else {
-            $this->presenter->redirect('this');
-        }
-
-    }
 }
