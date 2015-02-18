c382b60b6d522eebe388f79e8518a6ab04e326da
Josef Kolář <thejoeejoee@gmail.com>
2015-02-18 23:10:45 +0100

    hotfix

--- minicup/AdminModule/presenters/HomepagePresenter.php
+++ minicup/AdminModule/presenters/HomepagePresenter.php
@@ -3,8 +3,8 @@
 namespace Minicup\AdminModule\Presenters;
 
 use Minicup\Components\IPhotoUploadComponentFactory;
+use Minicup\Components\PhotoUploadComponent;
 use Minicup\Model\Manager\ReorderManager;
-use Minicup\Model\Repository\MatchRepository;
 use Minicup\Model\Repository\TagRepository;
 
 /**
@@ -14,15 +14,16 @@ class HomepagePresenter extends BaseAdminPresenter
 {
     /** @var ReorderManager @inject */
     public $reorder;
-    /** @var MatchRepository @inject */
-    private $MR;
 
-    /** @var  IPhotoUploadComponentFactory @inject */
+    /** @var IPhotoUploadComponentFactory @inject */
     public $PUC;
 
     /** @var  TagRepository @inject */
     public $TR;
 
+    /**
+     * @return PhotoUploadComponent
+     */
     public function createComponentPhotoUploadComponent()
     {
         return $this->PUC->create();
--- minicup/FrontModule/presenters/HomepagePresenter.php
+++ minicup/FrontModule/presenters/HomepagePresenter.php
@@ -2,21 +2,11 @@
 
 namespace Minicup\FrontModule\Presenters;
 use Minicup\Components\IPhotoUploadComponent;
-use Minicup\Model\Repository\TagRepository;
 
 /**
  * Homepage presenter.
  */
 final class HomepagePresenter extends BaseFrontPresenter
 {
-    /** @var  IPhotoUploadComponent @inject */
-    public $PUC;
 
-    /** @var  TagRepository @inject */
-    public $TR;
-
-    public function createComponentPhotoUploadComponent()
-    {
-        return $this->PUC->create();
-    }
 }
