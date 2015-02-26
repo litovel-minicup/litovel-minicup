3d98a8d6fc77be791c6e1c7d4a134413853b3221
Josef Kolář <thejoeejoee@gmail.com>
2015-02-26 22:15:30 +0100

    added transaction to match confirming

--- minicup/model/Manager/MatchManager.php
+++ minicup/model/Manager/MatchManager.php
@@ -3,6 +3,7 @@
 namespace Minicup\Model\Manager;
 
 
+use LeanMapper\Connection;
 use Minicup\Model\Entity\Match;
 use Minicup\Model\Repository\MatchRepository;
 use Nette\Object;
@@ -21,12 +22,16 @@ class MatchManager extends Object
     /** @var ReorderManager */
     private $RM;
 
-    function __construct(MatchRepository $MR, TeamDataRefresher $TDR, TeamReplicator $replicator, ReorderManager $RM)
+    /** @var Connection */
+    private $connection;
+
+    public function __construct(MatchRepository $MR, TeamDataRefresher $TDR, TeamReplicator $replicator, ReorderManager $RM, Connection $connection)
     {
         $this->MR = $MR;
         $this->TDR = $TDR;
         $this->replicator = $replicator;
         $this->RM = $RM;
+        $this->connection = $connection;
     }
 
 
@@ -34,17 +39,25 @@ class MatchManager extends Object
      * @param Match $match
      * @param $scoreHome
      * @param $scoreAway
+     * @throws \Exception
      * @return Match
      */
     public function confirmMatch(Match $match, $scoreHome, $scoreAway)
     {
-        $category = $match->category;
-        $match->scoreHome = $scoreHome;
-        $match->scoreAway = $scoreAway;
-        $match->confirmed = 1;
-        $this->MR->persist($match);
-        $this->replicator->replicate($category, $match);
-        $this->TDR->refreshData($category);
-        $this->RM->reorder($category);
+        $this->connection->begin();
+        try {
+            $category = $match->category;
+            $match->scoreHome = $scoreHome;
+            $match->scoreAway = $scoreAway;
+            $match->confirmed = 1;
+            $this->MR->persist($match);
+            $this->replicator->replicate($category, $match);
+            $this->TDR->refreshData($category);
+            $this->RM->reorder($category);
+        } catch (\Exception $e) {
+            $this->connection->rollback();
+            throw $e;
+        }
+        $this->connection->commit();
     }
 }
