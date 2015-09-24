fca0187ab95f34b90a42e08706365063224bc0c2
Josef Kolář <thejoeejoee@gmail.com>
2015-09-24 22:29:46 +0200

    removed migrate form from administration

--- minicup/AdminModule/presenters/HomepagePresenter.php
+++ minicup/AdminModule/presenters/HomepagePresenter.php
@@ -1,16 +1,9 @@
 <?php
 
 namespace Minicup\AdminModule\Presenters;
-use Minicup\Components\IMigrateFormComponentFactory;
 
 /**
  * Homepage presenter.
  */
 final class HomepagePresenter extends BaseAdminPresenter {
-    /** @var IMigrateFormComponentFactory @inject */
-    public $migrateFormFactory;
-
-    public function createComponentMigrateForm() {
-        return $this->migrateFormFactory->create();
-    }
 }
