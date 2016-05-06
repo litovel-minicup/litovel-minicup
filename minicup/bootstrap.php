8f653e9eebf6f65efd1f03f856c9f0131946424b
Josef Kolář <thejoeejoee@gmail.com>
2016-05-06 19:55:04 +0200

    updated dependencies, fixed title block

--- minicup/bootstrap.php
+++ minicup/bootstrap.php
@@ -17,7 +17,6 @@ $configurator->createRobotLoader()
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
-
 $container = $configurator->createContainer();
 
 return $container;
