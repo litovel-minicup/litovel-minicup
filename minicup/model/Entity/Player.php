b1502a20be06f34744430c10c007cd27d40b613a
Josef Kolář <thejoeejoee@gmail.com>
2018-03-12 23:40:27 +0100

    Fix TeamInfo relation name in Player

--- minicup/model/Entity/Player.php
+++ minicup/model/Entity/Player.php
@@ -15,7 +15,7 @@ namespace Minicup\Model\Entity;
  * @property string      $surname                       Surname of player
  * @property int         $number                        Ordinal number of player in team
  * @property int         $secondaryNumber               Secondary Ordinal number of player in team
- * @property TeamInfo    $teamInfoId m:hasOne           Info about player team
+ * @property TeamInfo    $teamInfo m:hasOne             Info about player team
  */
 class Player extends BaseEntity
 {
